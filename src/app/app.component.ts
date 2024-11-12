299aea68dd6243be61f266c1cea6343e8cc06658
Marcos Aguilar <marcosaguilar2001mal@gmail.com>
2024-11-12 15:49:06 +0100

    Proyecto terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {UserComponent} from './user/user.component';
+import {CommentsComponent} from './comments/comments.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent],
-  template:`<h1>Hola {{ city.toUpperCase() }}</h1>
-  <app-user></app-user>`,
-  styles: ``
+  imports: [RouterOutlet, UserComponent, CommentsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: `./app.component.css`
 })
 export class AppComponent {
   city = 'Barcelona'
--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -5,11 +5,51 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      comments works!
-    </p>
+    <h3>Comentarios varios</h3>
+    <img alt="photo" src="https://img2.rtve.es/i/?w=1600&i=1614352806474.png"/>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. cras diam mauris, hendrerit sed volutpat nec, tempor sit
+      amet arcu. etiam ut lacus augue. praesent vel neque sollicitudin, ornare ante ut, vestibulum ante. cras neque
+      risus, tincidunt vel dolor eu, placerat sodales augue. maecenas vitae condimentum sapien, mattis consequat nibh.
+      sed ligula velit, suscipit quis molestie nec, ornare eu metus. curabitur ac nisi elementum, lacinia massa a,
+      sollicitudin enim. integer id libero eu arcu hendrerit tempus. in hac habitasse platea dictumst. suspendisse
+      potenti. vestibulum quis magna eget mauris faucibus venenatis quis at turpis.
+
+      proin in malesuada ante. duis eu nisi finibus, posuere nibh quis, aliquam dui. morbi nulla mi, laoreet nec pretium
+      et, convallis vel arcu. integer efficitur, ligula quis congue dapibus, est nulla efficitur tellus, et imperdiet
+      quam urna vel neque. morbi convallis nec risus ut accumsan. vestibulum ante ipsum primis in faucibus orci luctus
+      et ultrices posuere cubilia curae; duis elementum libero sed sem vehicula, vitae condimentum tortor tempus.
+      curabitur quam sem, fermentum at imperdiet at, accumsan id orci. aenean eget aliquam massa. aenean varius, nisl
+      eget venenatis pharetra, urna lorem dignissim mauris, vehicula vehicula erat lacus eu est. aliquam felis massa,
+      accumsan nec lorem id, eleifend dignissim orci. praesent volutpat dolor sit amet mi convallis ultricies. in hac
+      habitasse platea dictumst. nunc vestibulum gravida tincidunt.
+
+      nam ut iaculis justo. cras erat purus, sodales ut maximus eu, varius quis sapien. sed nulla quam, volutpat
+      eleifend auctor id, pellentesque ac sem. ut pellentesque, nisl eu lobortis blandit, ipsum metus condimentum
+      ligula, vel sollicitudin lacus mauris id odio. ut ullamcorper dapibus ipsum, id lacinia mi. nullam gravida dolor
+      eget ex blandit facilisis. duis venenatis odio nec velit volutpat, at tincidunt quam bibendum. nam ultrices, justo
+      sit amet tempor finibus, urna tellus volutpat lectus, in cursus enim lacus vel ex. fusce nulla libero, malesuada
+      non imperdiet id, laoreet non arcu. praesent in pretium ipsum. maecenas at ipsum arcu. integer non purus eu nunc
+      interdum interdum. phasellus at gravida ante. pellentesque eu mauris nec leo porttitor tincidunt sit amet quis
+      libero.
+
+      nullam venenatis nibh quis felis hendrerit, non ultrices dui elementum. sed convallis nec enim nec auctor.
+      praesent aliquam nibh quis tellus bibendum semper. integer aliquet erat nec lorem bibendum facilisis. suspendisse
+      ultricies mi est, et venenatis justo blandit vitae. quisque varius purus velit, sit amet lobortis enim venenatis
+      sed. quisque orci nunc, pulvinar et lorem sed, consectetur lobortis mi. aenean consequat ipsum ut ipsum fermentum
+      fermentum vel a metus. sed scelerisque risus iaculis ornare tincidunt. aliquam fringilla enim diam, eu dapibus dui
+      facilisis sit amet. praesent feugiat mauris eros, vitae vulputate enim sodales a. donec in fermentum justo, vel
+      gravida ante. phasellus efficitur mauris nisi. quisque sed sapien eu tellus imperdiet aliquam eget in magna.
+
+      vestibulum imperdiet pellentesque tempor. praesent consequat, nunc vitae accumsan mollis, elit dui rhoncus massa,
+      sit amet pharetra enim felis ut orci. nunc eu nisl eget augue rhoncus interdum non at nunc. nunc vitae magna
+      aliquam, accumsan ex sit amet, condimentum ipsum. sed iaculis enim id dolor vulputate mollis. nunc metus turpis,
+      iaculis vel gravida in, condimentum in mi. vestibulum iaculis velit porttitor consequat dapibus. cras consequat
+      felis vitae tellus eleifend, sed interdum arcu bibendum. maecenas eu lorem sapien.</p>
   `,
-  styles: ``
+  styles: `img {
+    width: 100%;
+    height: auto;
+  }`
 })
 export class CommentsComponent {
 
